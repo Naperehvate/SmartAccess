5fc6ada51bf3e191800db8e2ade070fd9f1124f6
Naperehvate <tropikano647@mail.ru>
2025-01-23 17:09:31 +0500

    deploying the flask server

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 
+import os
+import subprocess
 from config import initialize_system
 
 def main():
+    subprocess.Popen(["python", '../../SmartAccess/web_app/app.py'])
     initialize_system()
 
 if __name__ == "__main__":
---
+++ web_app/app.py
@@ -0,0 +1,58 @@
+import sys
+import os
+
+from flask import Flask, render_template, request, jsonify
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from src.DB.database_manager import add_user, delete_user, get_all_users
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/admin_menu')
+def admin_menu():
+    return render_template('admin_menu.html')
+
+@app.route('/user_list')
+def user_list():
+    return render_template('user_list.html')
+
+@app.route('/get_users')
+def get_users():
+    try:
+        users = get_all_users()  # Получаем список пользователей из базы данных
+        return jsonify(users)
+    except Exception as e:
+        return jsonify({"error": f"Ошибка загрузки пользователей: {str(e)}"}), 500
+
+@app.route('/add_user', methods=['POST'])
+def add_user_route():
+    try:
+        data = request.get_json()
+        card_id = data['card_id']
+        name = data['name']
+        access_level = int(data['access_level'])
+
+        add_user(card_id, name, access_level)  # Добавляем пользователя в базу данных
+        return jsonify({"message": "Пользователь успешно добавлен!"})
+    except Exception as e:
+        return jsonify({"error": f"Ошибка добавления пользователя: {str(e)}"}), 500
+
+@app.route('/delete_user', methods=['POST'])
+def delete_user_route():
+    try:
+        data = request.get_json()
+        card_id = data['card_id']
+
+        delete_user(card_id)  # Удаляем пользователя из базы данных
+        return jsonify({"message": "Пользователь успешно удален!"})
+    except Exception as e:
+        return jsonify({"error": f"Ошибка удаления пользователя: {str(e)}"}), 500
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=5000, debug=True)
