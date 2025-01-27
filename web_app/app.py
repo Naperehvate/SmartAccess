88f343fad1e961b6a0b7e0ffea9147c030bd7200
Naperehvate <tropikano647@mail.ru>
2025-01-27 15:47:09 +0500

    Update app.py

--- web_app/app.py
+++ web_app/app.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify # type: ignore
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.DB.database_manager import *
